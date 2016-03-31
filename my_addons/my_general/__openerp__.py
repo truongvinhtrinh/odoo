43b5cf6016921a62062bbbc6c5245b402bdff152
truongvinhtrinh <vinhtrinh02dn@gmail.com>
2016-03-31 09:07:44 +0700

    add my_general module

--- my_addons/my_general/__openerp__.py
+++ my_addons/my_general/__openerp__.py
@@ -5,24 +5,17 @@
     'name': 'Trinh General Customize',
     'version': '1.0',
     'category': 'Sales Management',
-    'sequence': 8000,
+    'sequence': 15,
     'summary': 'Trinh general customize',
     'description': """
     To:
-    -- invisible in Product Form:
-        Fields:
-            invoicing_policy
-            to_weight fields
-        Tabs:
-            Inventory tab
-            Accounting
-            Notes
+    -- invisible invoicing_policy, to_weight
+        fields in product form
     """,
     'website': 'https://www.odoo.com',
-    'depends': ['point_of_sale', 'sale', 'product', 'account'],
+    'depends': ['sale'],
     'data': [
         'views/sale_view.xml',
-        'views/account_view.xml',
     ],
     'demo': [],
     'css': [],
